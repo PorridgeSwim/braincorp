1fa370015b78081b3298fe749e7555ebd62dd767
PorridgeSwim <yz3883@columbia.edu>
2024-01-30 21:12:58 -0800

    write transpose test

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -23,12 +23,24 @@ void testMatrixMultiplication() {
     Matrix mat4(2, 2, {58, 64, 139, 154});
     assert(product.isEqualTo(mat4));
 
-    std::cout << "Multiplication test passed." << std::endl;
+    std::cout << "Matrix multiplication test passed." << std::endl;
+}
+
+void testTranspose() {
+
+    Matrix mat1(2, 3, {1, 2, 3, 4, 5, 6});
+    Matrix mat2(3, 2, {1, 4, 2, 5, 3, 6});
+    assert(mat1.isEqualTo(mat2) == false);
+    mat1.transpose();
+    assert(mat1.isEqualTo(mat2) == true);
+
+    std::cout << "Transpose test passed." << std::endl;
 }
 
 int main()
 {
     testMatrixMultiplication();
+    testTranspose();
     std::cout << "All tests passed successfully." << std::endl;
     return 0;
 }
